41f61ce6aee197074b13a797f5e92d8883df1139
nwparker <nwp7@cornell.edu>
2014-04-08 15:23:11 -0400

    Added xml mag and jagged arrays

--- Pathogenesis/Pathogenesis/Models/Map.cs
+++ Pathogenesis/Pathogenesis/Models/Map.cs
@@ -31,7 +31,7 @@ namespace Pathogenesis.Models
         // Size of the map tiles
         public const int TILE_SIZE = 40;
 
-        // Map dimensions
+        // Map dimensions in tiles
         public int Width { get; set; }
         public int Height { get; set; }
         public int WidthTiles
@@ -48,15 +48,21 @@ namespace Pathogenesis.Models
                 return Height / TILE_SIZE;
             }
         }
-
-        // Tile array representation of the map
-        private int[,] tiles;
+        public int[][] tiles;
 
         public Texture2D WallTexture { get; set; }
 
+        public Map() { }
+
         public Map(int width, int height, Texture2D wall_texture)
         {
-            tiles = new int[height/TILE_SIZE, width/TILE_SIZE];
+            int[][] tiles = new int[height / TILE_SIZE][];
+            for (int k = 0; k < tiles.Length; k++)
+            {
+                tiles[k] = new int[width / TILE_SIZE];
+            }
+            //tiles[0].GetLength(0)
+            // [width/TILE_SIZE]
             Width = width;
             Height = height;
 
@@ -66,73 +72,68 @@ namespace Pathogenesis.Models
             setTiles(tiles);
         }
 
-        public void setTiles(int[,] tiles)
+        public void setTiles(int[][] tiles)
         {
             this.tiles = tiles;
-            for (int i = 0; i < tiles.GetLength(0); i++)
+            for (int i = 0; i < tiles.Length; i++)
             {
-                for (int j = 0; j < tiles.GetLength(1); j++)
+                for (int j = 0; j < tiles[0].Length; j++)
                 {
                     //Map Borders
-                    if (i == 0 || j == 0 || i == tiles.GetLength(0)-1 || j == tiles.GetLength(1)-1)
+                    if (i == 0 || j == 0 || i == tiles.Length - 1 || j == tiles[0].Length - 1)
                     {
-                        tiles[i, j] = 1;
-                    }
-
-                    if (i == 5 && j > 3 && j < 7)
-                    {
-                        tiles[i, j] = 1;
+                        tiles[i][j] = 1;
                     }
 
                     //Test
                     if (i == 10 && j > 5 && j < 7)
                     {
-                        tiles[i, j] = 1;
+                        tiles[i][j] = 1;
                     }
 
                     if (i == 15 && j > 15 && j < 20)
                     {
-                        tiles[i, j] = 1;
+                        tiles[i][j] = 1;
                     }
 
                     if (i == 20 && j > 40 && j < 60)
                     {
-                        tiles[i, j] = 1;
+                        tiles[i][j] = 1;
                     }
 
                     if (j == 40 && i > 21 && i < 40)
                     {
-                        tiles[i, j] = 1;
+                        tiles[i][j] = 1;
                     }
 
                     if (j == 10 && i > 18 && i < 27)
                     {
-                        tiles[i, j] = 1;
+                        tiles[i][j] = 1;
                     }
 
                     if (i == 40 && j > 50 && j < 70)
                     {
-                        tiles[i, j] = 1;
+                        tiles[i][j] = 1;
                     }
 
                     if (i == 40 && j > 80 && j < 100)
                     {
-                        tiles[i, j] = 1;
+                        tiles[i][j] = 1;
                     }
 
-                    if (j == 10 && i > 10 && i < 18)
+                    if (j == 10 && i > 10 && i < 15)
                     {
-                        tiles[i, j] = 1;
+                        tiles[i][j] = 1;
                     }
 
                     if (j == 80 && i > 10 && i < 15)
                     {
-                        tiles[i, j] = 1;
+                        tiles[i][j] = 1;
                     }
 
                     if (j == 80 && i > 25 && i < 35)
                     {
-                        tiles[i, j] = 1;
+                        tiles[i][j] = 1;
                     }
 
 
@@ -140,11 +141,13 @@ namespace Pathogenesis.Models
             }
         }
 
+
+
         #region Map Methods
         public int getTileAt(int x, int y)
         {
             if (!canMoveTo(x, y)) return -1;
-            return tiles[y, x];
+            return tiles[y][x];
         }
 
         public Vector2 translateWorldToMap(Vector2 worldPos)
@@ -154,16 +157,14 @@ namespace Pathogenesis.Models
 
         public bool canMoveTo(int x, int y)
         {
-            return x >= 0 && y >= 0 && x < tiles.GetLength(1) && y < tiles.GetLength(0) &&
-                tiles[y, x] != 1;
+            return x >= 0 && y >= 0 && y < tiles.Length && x < tiles[0].Length &&
+                tiles[y][x] != 1;
         }
 
         public bool canMoveToWorldPos(Vector2 position)
         {
-            float x = position.X / TILE_SIZE;
-            float y = position.Y / TILE_SIZE;
-            return x >= 0 && y >= 0 && x < tiles.GetLength(1) && y < tiles.GetLength(0) &&
-                tiles[(int)y, (int)x] != 1;
+            Vector2 worldPos = translateWorldToMap(position);
+            return canMoveTo((int)worldPos.X, (int)worldPos.Y);
         }
 
         /*
@@ -173,7 +174,7 @@ namespace Pathogenesis.Models
         {
             Vector2 diff = end - start;
 
-            if ((int)start.X/TILE_SIZE == (int)end.X/TILE_SIZE && (int)start.Y/TILE_SIZE == (int)end.Y/TILE_SIZE
+            if ((int)start.X / TILE_SIZE == (int)end.X / TILE_SIZE && (int)start.Y / TILE_SIZE == (int)end.Y / TILE_SIZE
                 || !canMoveToWorldPos(start) || !canMoveToWorldPos(end) || diff.Length() < Map.TILE_SIZE)
             {
                 return true;
@@ -210,7 +211,7 @@ namespace Pathogenesis.Models
             float b = y0 - slope * x0;
 
             // Casts two lines account for the cast width
-            for (int i = x0; i <= x1; i+=TILE_SIZE/4)
+            for (int i = x0; i <= x1; i += TILE_SIZE / 4)
             {
                 int x_offset = (int)(cast_width / 2 * Math.Cos(Math.Atan(-1 / slope)));
                 int y_offset = (int)(cast_width / 2 * Math.Sin(Math.Atan(-1 / slope)));
@@ -224,8 +225,8 @@ namespace Pathogenesis.Models
                 int y_bot = (int)(slope * i + b - y_offset);
                 float x2 = steep ? y_bot : x_bot;
                 float y2 = steep ? x_bot : y_bot;
-                if (tiles[(int)(y / TILE_SIZE), (int)(x / TILE_SIZE)] == 1 ||
-                    tiles[(int)(y2 / TILE_SIZE), (int)(x2 / TILE_SIZE)] == 1)
+                if (tiles[(int)(y / TILE_SIZE)] [(int)(x / TILE_SIZE)] == 1 ||
+                    tiles[(int)(y2 / TILE_SIZE)] [(int)(x2 / TILE_SIZE)] == 1)
                 {
                     return true;
                 }
@@ -238,8 +239,9 @@ namespace Pathogenesis.Models
         #region Draw
         public void Draw(GameCanvas canvas)
         {
-            for(int i = 0; i < tiles.GetLength(0); i++) {
-                for (int j = 0; j < tiles.GetLength(1); j++)
+            for (int i = 0; i < tiles.Length; i++)
+            {
+                for (int j = 0; j < tiles[0].Length; j++)
                 {
                     //canvas.DrawText(i + "," + j, Color.White, new Vector2(j * TILE_SIZE, i * TILE_SIZE));
 
@@ -248,11 +250,11 @@ namespace Pathogenesis.Models
                         Vector2 next = Pathfinder.pointLocMap[i, j];
                         canvas.DrawText((int)next.Y + "," + (int)next.X, Color.White, new Vector2(j * TILE_SIZE, i * TILE_SIZE));
                     }
-                    
-                    if (tiles[i, j] == 1)
+
+                    if (tiles[i][j] == 1)
                     {
                         canvas.DrawSprite(WallTexture, Color.White,
-                            new Rectangle(j*TILE_SIZE, i*TILE_SIZE, TILE_SIZE, TILE_SIZE),
+                            new Rectangle(j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE),
                             new Rectangle(0, 0, WallTexture.Width, WallTexture.Height));
                     }
                 }
